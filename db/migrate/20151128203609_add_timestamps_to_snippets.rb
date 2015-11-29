6fb989a72274f4f08aa18223a116d4c205a2586d
Vitaly Shvedchenko <vshvedchenko@npoprogress.com>
2015-11-29 15:57:29 +0300

    one more migrations fix for heroku

--- db/migrate/20151128203609_add_timestamps_to_snippets.rb
+++
@@ -1,6 +0,0 @@
-class AddTimestampsToSnippets < ActiveRecord::Migration
-  def change
-    add_column :snippets, :created_at, :timestamp
-    add_column :snippets, :updated_at, :timestamp
-  end
-end
